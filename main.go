96212201ecc36bf9c3e3b629a02b90257b4c7b91
jojo <jojo@gmail.com>
2023-03-21 18:26:16 +0800

    feat: 打印输出方法

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"awesomeProject/test"
-	"awesomeProject/test/cmd"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	test.Name("main-name-test-git3")
-	//我实现了一个用户管理功能
-	test.Name("main-name-test-git2")
-	test.Age()
-	//我实现了另外一个功能
-	fmt.Println("1111")
-	cmd.CmdTest()
-	println()
+	fmt.Println("Hello")
 }
